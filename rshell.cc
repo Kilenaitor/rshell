a38e21e3f1e3ff62d622f868bc98bc94a4707cba
Kyle Minshall <kyleminshall@gmail.com>
2015-04-08 23:05:45 -0700

    Inital threading progress

--- rshell.cc
+++ rshell.cc
@@ -1,23 +1,44 @@
 #include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include "errno.h"
 #include <string>
 #include "boost/tokenizer.hpp"
 
 using namespace std;
 using namespace boost;
 
-int main() {
-	
+int main (int argc, char const *argv[])
+{   
+    char host[128];
+    gethostname(host, sizeof(host));
+    char * login = getlogin();
+    
 	string input;
-	
-	while(getline(cin, input))
+    
+    while(true)
 	{
-		if(input == "ls")
-			cout << "You typed ls" << endl;
-		if(input == "exit")
-		{
-			cout << "Bye" << endl;
-			sleep(1);
-			break;
-		}
+        //Terminal prompt display
+        cout << login << "@" << host << " $ ";
+        cout.flush();
+
+        //Grab user input for bash prompt
+        getline(cin,input);
+    
+        if(argv[1] == "exit")
+            break;
+    
+        //Main process thread
+        pid_t i = fork();
+        if(i == 0) 
+        {
+            char* arg[] = {"ls", "-l", NULL};
+            /*int com = execvp(arg[0], arg);
+            if(com < 0)
+                perror("Error executing command.");
+            */
+            cout << "Made it here" << endl;
+        }
+    
 	}
 }
