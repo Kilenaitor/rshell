e7da8217e8941ac0201a6efb59a6dfa2ff546253
Kyle Minshall <kyleminshall@gmail.com>
2015-04-09 02:28:40 -0700

    Updated error output to look like bash shell

--- rshell.cc
+++ rshell.cc
@@ -26,14 +26,23 @@ int main (int argc, char const *argv[])
         //Grab user input for bash prompt
         getline(cin,input);
         
+        //Setting up a boost tokenizer. 
         typedef boost::tokenizer<boost::char_separator<char> > tokenizer;
-        boost::char_separator<char> sep(" ", ";");
+        boost::char_separator<char> sep(" ", ";"); //' ' (space) as delmieter
         tokenizer tok(input, sep);
         
+        //ls is a temp list for storing the tokenized value
         list<string> ls;
+        //vector for storing a command and its arguments
         vector<char*> args;
+        //vector for storing all of the commands that have been chained together
         vector<vector<char*> > commands;
         
+        /*
+        Check for # and immediatly end parsing since anything after it is a comment.
+        Check for ; or || or && to know if a new command needs to be created.
+        Put all of these new commands in the commands vector.
+        */
         for(tokenizer::iterator it = tok.begin(); it != tok.end(); ++it) {
             if(*it == "#")
                 break;
@@ -50,10 +59,13 @@ int main (int argc, char const *argv[])
         args.push_back(0);
         commands.push_back(args);
         
+        //Go through all of the commands
         for(int x = 0; x < commands.size(); x++) {
             
+            //Get the current command
             vector<char*> com = commands.at(x);
             
+            //Using string compare here since they're char * entries
             if(strncmp(com[0], "exit", 4) == 0) {
                 exit(0);
             }
@@ -68,8 +80,13 @@ int main (int argc, char const *argv[])
             else if(i == 0) { //Child process
             
                 int result = execvp(com[0], &com[0]);
-                if(result < 0)
-                    perror("Error executing command");
+                if(result < 0) {
+                    char result[100];
+                    const char *error = "-rshell: ";
+                    strcpy(result, error);
+                    strcat(result, com[0]);
+                    perror(result);
+                }
                 else {
                     exit(0);
                 }
