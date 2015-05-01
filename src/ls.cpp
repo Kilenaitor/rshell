2768dbfa860d120e67c7e82a1a78b7aea5743a07
Kyle Minshall <kyleminshall@gmail.com>
2015-05-01 14:13:44 -0700

    Fixing formatting

--- src/ls.cpp
+++ src/ls.cpp
@@ -30,7 +30,7 @@
 
 using namespace std;
 
-void list_output(vector<char*> &v)
+void list_output(vector<char*> &v, const char* input)
 {
 	struct passwd *pws;
 	pws = getpwuid(geteuid());
@@ -39,26 +39,42 @@ void list_output(vector<char*> &v)
 	grp = getgrgid(getgid());
 	
 	int width = 0;
+	int width2 = 0;
 	int total = 0;
 	
+	
 	for(auto x : v) {
+		
+		string full = input;
+		full.append("/");
+		full.append(x);
+		
+		
 		int tempWidth = 0;
+		int tempWidth2 = 0;
 		struct stat fileStat;
-		if(stat(x, &fileStat) < 0)
-			perror("Failed");
+		if(stat(full.c_str(), &fileStat) < 0)
+			perror("Failed to run filestat");
 		
 		total += ceil(fileStat.st_blocks);
 		
 		for(; fileStat.st_size != 0; fileStat.st_size /= 10, tempWidth++);
+		for(; fileStat.st_nlink != 0; fileStat.st_nlink /= 10, tempWidth2++);
 		tempWidth > width ? width = tempWidth : width;
+		tempWidth2 > width2 ? width2 = tempWidth2 : width2;
 	}
 	
 	cout << "total " << total/2 << endl;
 	
     for(auto x : v) {
+		
+		string full = input;
+		full.append("/");
+		full.append(x);
+		
 		struct stat fileStat;
-	    if(stat(x, &fileStat) < 0)  
-			perror("Failed");
+	    if(stat(full.c_str(), &fileStat) < 0)  
+			perror("Failed to do filestat");
         
         bool directory = (S_ISDIR(fileStat.st_mode));
         bool executable = (fileStat.st_mode > 0) && (S_IEXEC & fileStat.st_mode);
@@ -75,6 +91,7 @@ void list_output(vector<char*> &v)
 	    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
 	    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
 		printf("  ");
+		cout.width(width2);
         cout << fileStat.st_nlink << " ";
 		printf("%s  ", pws->pw_name);
 		printf("%s  ", grp->gr_name);
@@ -106,7 +123,7 @@ void list_output(vector<char*> &v)
 	}
 }
 
-void standard_output(vector<char*> &v, int length)
+void standard_output(vector<char*> &v, int length, const char* source)
 {
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
@@ -116,8 +133,14 @@ void standard_output(vector<char*> &v, int length)
     for(int i = 0; i < num_rows; ++i)
     {
         for(unsigned a = i; a < v.size(); a += num_rows) {
+			
+			string full = source;
+			full.append("/");
+			full.append(v.at(i));
             
     		struct stat fileStat;
+		    if(stat(full.c_str(), &fileStat) < 0)  
+				perror("Failed to do filestat");
             
             bool directory = (S_ISDIR(fileStat.st_mode));
             bool executable = (fileStat.st_mode > 0) && (S_IEXEC & fileStat.st_mode);
@@ -154,63 +177,101 @@ bool comparisonFunc(const char *c1, const char *c2) {
 int main (int argc, char const *argv[])
 {
     DIR *dirp;
-    string direc = "./";
+    string direc = ".";
     bool list = false;
     bool all = false;
     bool recursive = false;
     vector<char*> files;
     unsigned max_length = 9;
+	string flags = "";
+	vector<const char*> inputs;
+	
+	for(int i = 1; i < argc; i++) {
+		if(argv[i][0] == '-')
+			flags += argv[i];
+		else
+			inputs.push_back(argv[i]);
+	}
+	
+    if(flags.find('a') != string::npos)
+        all = true;
+    if(flags.find('l') != string::npos)
+        list = true;
+    if(flags.find('R') != string::npos)
+        recursive = true;
+	
+	if(inputs.size() > 0) {
+	
+		for(auto in : inputs) {
     
-    if(argc == 2) {
-        string first = argv[1];
-        if(first[0] == '-') {
-            if(first.find('a') != string::npos)
-                all = true;
-            if(first.find('l') != string::npos)
-                list = true;
-            if(first.find('R') != string::npos)
-                recursive = true;
-        }
-        else
-            direc = argv[1];
-    }
+		    if(NULL == (dirp = opendir(const_cast<char*>(in)))) {
+		        perror("There was an error with opendir(). ");
+		        exit(1);
+		    }
+			
+		    struct dirent *filespecs;
+		    errno = 0;
+		    while(NULL != (filespecs = readdir(dirp))) {
+		        if(filespecs->d_name[0] == '.' && !all)
+		            continue;
+		        files.push_back(filespecs->d_name);
+		        strlen(filespecs->d_name) > max_length ? max_length = strlen(filespecs->d_name) : max_length;
+		    }
+		    if(errno != 0) {
+		        perror("There was an error with readdir(). ");
+		        exit(1);
+		    }
     
-    if(argc > 2) {
-        direc = argv[2];
-    }
+		    sort(files.begin(), files.end(), comparisonFunc);
+			
+		    if(!list)
+		        standard_output(files, ++max_length, in);
+		    else
+		        list_output(files, in);
     
-    if(NULL == (dirp = opendir(const_cast<char*>(direc.c_str())))) {
-        perror("There was an error with opendir(). ");
-        exit(1);
-    }
+		    if(recursive)
+		        cout << "rec" << endl;
     
-    
-    struct dirent *filespecs;
-    errno = 0;
-    while(NULL != (filespecs = readdir(dirp))) {
-        if(filespecs->d_name[0] == '.' && !all)
-            continue;
-        files.push_back(filespecs->d_name);
-        strlen(filespecs->d_name) > max_length ? max_length = strlen(filespecs->d_name) : max_length;
-    }
-    if(errno != 0) {
-        perror("There was an error with readdir(). ");
-        exit(1);
-    }
-    
-    sort(files.begin(), files.end(), comparisonFunc);
-    
-    if(!list)
-        standard_output(files, ++max_length);
-    else
-        list_output(files);
-    
-    if(recursive)
-        cout << "rec" << endl;
-    
-    if(-1 == closedir(dirp)) {
-        perror("There was an error with closedir(). ");
-        exit(1);
-    }
+		    if(-1 == closedir(dirp)) {
+		        perror("There was an error with closedir(). ");
+		        exit(1);
+		    }
+		}
+	}
+	else {
+		
+	    if(NULL == (dirp = opendir(const_cast<char*>(direc.c_str())))) {
+	        perror("There was an error with opendir(). ");
+	        exit(1);
+	    }
+	
+	    struct dirent *filespecs;
+	    errno = 0;
+	    while(NULL != (filespecs = readdir(dirp))) {
+	        if(filespecs->d_name[0] == '.' && !all)
+	            continue;
+	        files.push_back(filespecs->d_name);
+	        strlen(filespecs->d_name) > max_length ? max_length = strlen(filespecs->d_name) : max_length;
+	    }
+	    if(errno != 0) {
+	        perror("There was an error with readdir(). ");
+	        exit(1);
+	    }
+
+	    sort(files.begin(), files.end(), comparisonFunc);
+
+	    if(!list)
+	        standard_output(files, ++max_length, direc.c_str());
+	    else
+	        list_output(files, direc.c_str());
+
+	    if(recursive)
+	        cout << "rec" << endl;
+
+	    if(-1 == closedir(dirp)) {
+	        perror("There was an error with closedir(). ");
+	        exit(1);
+	    }
+	}
     return 0;
 }
